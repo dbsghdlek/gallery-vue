43a8cf5e14ac9e6129f12b035fe7e8f70acee22e
yoonho <dbsghdlek2@naver.com>
2025-03-09 18:54:31 +0900

    갤러리 사진 표시 정상화(신창섭)

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,37 +1,59 @@
 <template>
   <div class="row">
-    <a v-for="(image, index) in images" :key="index" :href="image" class="glightbox col-4">
-      <img :src="image" class="img-thumbnail" />
+    <a v-for="(image, index) in images" :key="index" :href="image.fileUrl" class="glightbox col-4">
+      <img :src="image.fileUrl" class="img-thumbnail" width="800" height="800"/>
     </a>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, onUnmounted, ref } from 'vue'
+import { nextTick, onMounted, onUnmounted, ref, watch } from 'vue'
 import GLightbox from 'glightbox'
 import 'glightbox/dist/css/glightbox.css'
 import axios from 'axios'
 
 
 let lightbox: ReturnType<typeof GLightbox>
-const images = ['../../public/images/100.jpg', '../../public/images/soeun.jpg']
-// const images = ref([])
+// const images = ['http://localhost:3000/resource/100.jpg', 'http://localhost:3000/resource/test.jpg']
+const images = ref([])
+
 onMounted(async () => {
-  // await axios.get('http://localhost:3000/gallery/1').then(res => {
-  //   res.data.body.forEach(gallery => {
-  //     let image = new Uint8Array(gallery.file.data);
-  //     image = new Blob([image], {type : "image/jpg"})
-  //     image = URL.createObjectURL(image);
-  //     images.value.push(image)
-  //   })
-  // })
-  lightbox = GLightbox({ selector: '.glightbox', closeButton: true, width: 'auto', height: 'auto', draggable: true })
+  const galleryInfoList  = await axios.get('http://localhost:3000/gallery/1')
+  galleryInfoList.data.body.forEach((galleryInfo) => {
+    galleryInfo.fileUrl = `http://localhost:3000/resource/${galleryInfo.fileName}`
+  })
+  images.value = galleryInfoList.data.body;
+})
+
+watch(images, async () => {
+  if (lightbox) {
+    lightbox.destroy() // 기존 인스턴스 제거
+  }
+  await nextTick()
+
+  lightbox = GLightbox({
+    selector: '.glightbox',
+    closeButton: true,
+    draggable: true,
+    width: 'auto',  // 고정된 크기 대신 자동 크기 조절
+    height: 'auto',
+  })
 })
 
 onUnmounted(() => {
+  images.value.forEach(url => {
+    URL.revokeObjectURL(url)
+  })
   if (lightbox) lightbox.destroy()
 })
 </script>
 
 <style scoped>
+.glightbox img {
+  max-width: 100%;
+  height: auto;
+  object-fit: contain;
+  display: block;
+  margin: auto;
+}
 </style>
